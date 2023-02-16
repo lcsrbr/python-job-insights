d37a22763956efdeb2b9440c13bd7bd84dfe86a7
Lucas Moura <93lucasribeiro@gmail.com>
2023-02-16 14:54:39 -0300

    req3 - buscar por industria

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,14 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    jobs_list = read(path)
+    arr = []
+    for row in jobs_list:
+        if row["industry"] not in arr and len(row["industry"]):
+            arr.append(row["industry"])
+    return arr
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
