33b0d4209b2784e2ed447f3983e44f59f269bd1d
Lucas Moura <93lucasribeiro@gmail.com>
2023-02-16 17:54:07 -0300

    req10 - test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+
+path = "tests/mocks/jobs.csv"
 
 
 def test_counter():
-    pass
+    assert count_ocurrences(path, "back") == 1
+    assert count_ocurrences(path, "front") == 1
+    assert count_ocurrences(path, "full") == 3
+    assert count_ocurrences(path, "trainee") == 1
