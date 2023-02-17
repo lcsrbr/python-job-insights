c7bbd2e1aef7e54ff50221df8dba20e3f2712e6e
Lucas Moura <93lucasribeiro@gmail.com>
2023-02-17 14:57:27 -0300

    req13 - sort by tests

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,18 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+from src.insights.jobs import read
+
+
+path = "tests/mocks/jobs_with_salaries_and_date_posted.csv"
 
 
 def test_sort_by_criteria():
-    pass
+    file = read(path)
+
+    sort_by(file, "min_salary")
+    assert file[0]["min_salary"] == "1000"
+
+    sort_by(file, "max_salary")
+    assert file[0]["max_salary"] == "8000"
+
+    sort_by(file, "date_posted")
+    assert file[0]["date_posted"] == "2021-06-01"
